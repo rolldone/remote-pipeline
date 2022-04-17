36ecde9313edf4bb7b5e5ef2c7bbb88e24cedfed
donny rolanda <donny.rolanda@gmail.com>
2022-04-18 07:06:13 +0800

    Create the page design for all route - Add authentication field for host  and the specifict host

---
+++ migrations/20220417225119_update_hosts.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('hosts', function (table) {
+    table.string("auth_type");
+    table.string("username");
+    table.text("password");
+    table.text("private_key");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('hosts', function (table) {
+    table.dropColumn("auth_type");
+    table.dropColumn("username");
+    table.dropColumn("password");
+    table.dropColumn("private_key");
+  });
+};
