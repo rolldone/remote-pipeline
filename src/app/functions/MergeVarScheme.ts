5cc569208cf48238f2eeadad1f4d69710d2e29c7
donny <donny.rolanda@gmail.com>
2022-07-14 10:28:12 +0800

    Improvement
    - [Function] : Add condition if empty var name give default value

--- src/app/functions/MergeVarScheme.ts
+++ src/app/functions/MergeVarScheme.ts
@@ -3,11 +3,13 @@ const MergeVarScheme = function (variable, scheme, extra = {}) {
     let jj = {};
     for (var a = 0; a < schema.length; a++) {
       // if (variable[a].is_active == true) {
+      let isFound = false;
       switch (schema[a].type) {
         case 'input-asset':
           for (let b in variable) {
             if (variable[b].name == schema[a].name) {
               jj[schema[a].name] = variable[a].attachment_datas;
+              isFound = true;
               break;
             }
           }
@@ -16,6 +18,7 @@ const MergeVarScheme = function (variable, scheme, extra = {}) {
           for (let b in variable) {
             if (variable[b].name == schema[a].name) {
               jj[schema[a].name] = variable[a].value;
+              isFound = true;
               break;
             }
           }
@@ -24,12 +27,26 @@ const MergeVarScheme = function (variable, scheme, extra = {}) {
           for (let b in variable) {
             if (variable[b].name == schema[a].name) {
               jj[schema[a].name] = variable[a].value;
+              isFound = true;
               break;
             }
           }
           break;
       }
       // }
+      if (isFound == false) {
+        switch (schema[a].type) {
+          case 'input-asset':
+            jj[schema[a].name] = schema[a].attachment_datas;
+            break;
+          case 'input-script':
+            jj[schema[a].name] = schema[a].value;
+            break;
+          case 'input-text':
+            jj[schema[a].name] = schema[a].value;
+            break;
+        }
+      }
     }
     return {
       ...jj,
